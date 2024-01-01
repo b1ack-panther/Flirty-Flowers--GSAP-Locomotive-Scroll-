486df873f4dfc1c4b71ff1aaf9cdd8d1291b9289
b1ack-panther <saurabh8604vns@gmail.com>
2024-01-01 10:53:57 +0530

    about bug

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -34,7 +34,7 @@ function About() {
     <section className={"about-section"} data-scroll>
       <SectionHeader title="About" />
 			<p id="headline">
-				Flirty Flowers is a blog about flowers and the designers who make them into art. Creativity and the art of 'making' require dialogue. The full purpose of the Flirty Flowers blog is to encourage and inspire. We value art, we value insight, and we value opinion.
+				Flirty Flowers is a blog about flowers and the designers who make them into art. Creativity and the art of 'making' require dialogue. The full purpose of the Flirty Flowers blog is to encourage and inspire. We value art, insight, and we value opinion.
 			</p>
 		</section>
 	);
