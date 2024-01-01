28c05e9375e66d1f615f1279133cebd54450dbe4
b1ack-panther <saurabh8604vns@gmail.com>
2024-01-02 00:43:42 +0530

    gallery bug fixes

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -9,7 +9,7 @@ function Featured() {
 		"https://images.unsplash.com/photo-1552248524-10d9a7e4841c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=881&q=100";
 
 	const imgRef1 = useRef(null);
-	let onScreen = useOnScreen(imgRef1);
+	let onScreen = useOnScreen(imgRef1, 0.5);
 	const imgRef2 = useRef(null);
 	let onScreen1 = useOnScreen(imgRef2);
 
